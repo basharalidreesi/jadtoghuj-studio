7747a193880a3db3dc1ac00d223f047f86455920
Bashar al-Idreesi <basharalidreesi@Bashars-MacBook-Pro.local>
2023-03-22 01:46:27 +0300

    u

--- sanity.config.ts
+++ sanity.config.ts
@@ -34,7 +34,6 @@ export default defineConfig({
 			logo: logo,
 			layout: (props) => hideElementUsingCssSelector(props, '[id^="noCreate-"] [data-ui="Button"][data-testid="action-intent-button"]'),
 			navbar: (props) => hideElementUsingCssSelector(props, `
-				#new-document-dialog [data-ui="Button"][href="/intent/create/template=look;type=look/"],
 				#new-document-dialog [data-ui="Button"][href="/intent/create/template=page;type=page/"],
 				#new-document-dialog [data-ui="Button"][href="/intent/create/template=settings;type=settings/"],
 				#new-document-dialog [data-ui="Button"][href="/intent/create/template=settings-for-jadtoghuj.com;type=settings/"]
